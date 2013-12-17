2e56ca11a227b7063100a7d94af180c3061417f9
anouer <anouer@anouer-OptiPlex-390.(none)>
2013-12-17 14:58:52 +0100

    odif de test

--- src/Ance/StarBundle/Controller/DefaultController.php
+++ src/Ance/StarBundle/Controller/DefaultController.php
@@ -9,5 +9,6 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return $this->render('AnceStarBundle:Default:index.html.twig', array('name' => $name));
+        
     }
 }
